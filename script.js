ffb0841ff95fd8c95cd0ffac7bea9e27ce75a341
reece.grundy <113994172+Reece-Grundy@users.noreply.github.com>
2024-02-13 18:53:14 +0000

    Start rock paper scissors

---
+++ script.js
@@ -0,0 +1,14 @@
+// determines the choice for the computer by using a random number generator
+function getComputerChoice() {
+    let num = Math.floor(Math.random() * 3);
+    // declare computer choice variable
+    let computerChoice;
+    // Conditional ternary statement to pick rock, paper or scissors
+    (num == 0) ? computerChoice = "Rock" :
+        (num ==1) ? computerChoice = "Paper" :
+            computerChoice = "Scissors";
+    console.log(computerChoice, num);
+}
+
+// calls the function 
+getComputerChoice();
